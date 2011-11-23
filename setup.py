df0fa799d9eed9c7cb18bfd84d394b445c8a2835
Bertrand Néron <bneron@pasteur.fr>
2011-11-23 12:40:37 +0000

    add a nohup_egg_info class to skipp egg info installation in site-package
    SVN-Revision: 18

--- setup.py
+++ setup.py
@@ -1,19 +1,29 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+from distutils.command.install_egg_info import install_egg_info as _install_egg_info
+
+class nohup_egg_info(_install_egg_info):
+  def run(self):
+    #there is nothing to install in sites-package
+    #so I don't put any eggs in it
+    pass
+
+
 
 setup(name        = 'blast2usa',
-      version     = '1.1',
+      version     = '1.2',
       author      = "Neron B",
       author_email = "bneron@pasteur.fr" ,
       license      = "GPLv2" ,
       description  = " parse a blast2 report ( text format -m 0 ) and return a list of hits identifier in usa format.",
       classifiers = [
-	             'License :: GPLv2' ,
+	                 'License :: GPLv2' ,
                      'Operating System :: POSIX' ,
                      'Programming Language :: Python' ,
                      'Topic :: Bioinformatics' ,
                     ] ,
       scripts     = [ 'src/blast2usa' ] ,
+      cmdclass = { 'install_egg_info': nohup_egg_info }
       )
 
