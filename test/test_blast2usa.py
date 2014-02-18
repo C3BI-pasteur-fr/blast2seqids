7982fda666f1fb31331a5ff65d08bfb903ecaae8
Bertrand Néron <bneron@pasteur.fr>
2014-02-18 17:15:19 +0100

    add support of pdb

--- test/test_blast2usa.py
+++ test/test_blast2usa.py
@@ -11,15 +11,16 @@ import unittest
 import os.path
 import sys
 
-data_path = os.path.abspath( os.path.join( os.path.dirname( __file__ ) , 'data') )
-from blast2usa import _searchBegining , parseSummary , format2usa
+from blast2usa import _searchBegining, parseSummary, format2usa, Entry
 
 
 class Test(unittest.TestCase):
 
 
     def setUp(self):
-        self.report_path = os.path.join(data_path, 'blast2_report.txt' )
+        self.data_dir = os.path.abspath( os.path.join( os.path.dirname( __file__ ) , 'data') )
+        self.report_path_generic_bank = os.path.join(self.data_dir, 'blast2_generic_report.txt' )
+        self.report_path_pdb = os.path.join(self.data_dir, 'blast2_pdb_report.txt')
         self.ids = [('sp', 'Q61285'), ('sp', 'Q9QY44'), ('sp', 'Q9UBJ2'), ('sp', 'P33897'), 
                     ('sp', 'P48410'), ('sp', 'P28288'), ('sp', 'P16970'), ('sp', 'P55096'), 
                     ('sp', 'Q8T8P3'), ('sp', 'P41909'), ('sp', 'P34230'), ('sp', 'Q94FB9'), 
@@ -32,34 +33,35 @@ class Test(unittest.TestCase):
 
 
     def testSearchBegining(self):
-        report = open( self.report_path ,'r' )
+        report = open( self.report_path_generic_bank ,'r' )
         report = _searchBegining( report )
         first_summary_line = report.next()
         report.close()
         self.assertEqual(first_summary_line, "sp|Q61285|ABCD2_MOUSE RecName: Full=ATP-binding cassette sub-fam...  1508   0.0  \n")
         
     def testParseSummary(self):
-        report = open( self.report_path ,'r' )
-        ids = parseSummary( report )
-        report.close()
-        self.assertEqual( ids , self.ids )
+        with open(self.report_path_generic_bank, 'r') as report:
+            ids = parseSummary(report)
+        self.assertEqual(ids, self.ids)
+         
+        with open(self.report_path_generic_bank, 'r') as report:
+            ids = parseSummary(report, From = 5 )
+        self.assertEqual(ids, self.ids[4: ] )
+         
+        with open(self.report_path_generic_bank, 'r') as report:
+            ids = parseSummary(report, To = 10)
+        self.assertEqual(ids, self.ids[: 10 ])
         
-        report = open( self.report_path ,'r' )
-        ids = parseSummary( report , From = 5 )
-        report.close()
-        self.assertEqual( ids , self.ids[4: ] )
+        with open(self.report_path_generic_bank, 'r') as report:
+            ids = parseSummary(report, From = 10, To = 15)
+        self.assertEqual(ids, self.ids[9: 15])
         
-        report = open( self.report_path ,'r' )
-        ids = parseSummary( report , To = 10 )
-        report.close()
-        self.assertEqual( ids , self.ids[: 10 ] )
-       
-        report = open( self.report_path ,'r' )
-        ids = parseSummary( report , From = 10 , To = 15 )
-        report.close()
-        self.assertEqual( ids , self.ids[9: 15] )
-        
+        with open(self.report_path_pdb, 'r') as report:
+            ids = parseSummary(report)
+        report_ids = [Entry('pdb', '4f4c'), Entry('pdb', '4lsg'), Entry('pdb', '4lsg'), Entry('pdb', '4ksd'), Entry('pdb', '4ksc') ]
+        self.assertEqual(ids[0:5], report_ids) 
+
     def testFormat2usa(self):
-        ids= [ ( "sp" , "P33311" ) , ( "gb", "Q5F364") , ( "embl" , "O89016" ) ]
+        ids= [ Entry("sp", "P33311"), Entry("gb", "Q5F364"), Entry("embl", "O89016") ]
         usa = "sp:P33311\ngb:Q5F364\nembl:O89016\n"
         self.assertEqual( usa , format2usa(ids) )
